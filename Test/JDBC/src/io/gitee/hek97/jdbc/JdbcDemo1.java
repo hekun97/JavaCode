5e45987eaa6fa23376a49b17641c49479e2bd21d
hekun97 <hekun97@outlook.com>
2021-07-01 16:37:16 +0800

    create some code

--- Test/JDBC/src/io/gitee/hek97/jdbc/JdbcDemo1.java
+++ Test/JDBC/src/io/gitee/hek97/jdbc/JdbcDemo1.java
@@ -2,6 +2,7 @@ package io.gitee.hek97.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class JdbcDemo1 {
@@ -10,17 +11,26 @@ public class JdbcDemo1 {
         //2. 注册驱动
         Class.forName("com.mysql.jdbc.Driver");
         //3. 获取数据库连接对象
-        Connection coon = DriverManager.getConnection("jdbc:mysql://localhost:3306/db3", "root", "root");
-        //4. 定义SQL语句
-        String sql = "update user set PASSWORD = 2345 where id = 1";
-        //5. 获取执行SQL对象 statement
+        Connection coon = DriverManager.getConnection("jdbc:mysql:///db3", "root", "root");
+        //4.1 定义第一条SQL语句
+        String sql1 = "update account set balance = 3500 where id = 1";
+        //4.2 定义第二条防注入SQL语句
+        String sql2 = "update account set balance = ? where id = ?";
+        //5.1 获取执行SQL对象 statement
         Statement stmt = coon.createStatement();
-        //5. 执行SQL
-        int cout = stmt.executeUpdate(sql);
-        System.out.println(cout);
+        //5.2 获取执行防注入SQL对象 prepareStatement
+        PreparedStatement ps = coon.prepareStatement(sql2);//注意：这里放SQL语句
+        ps.setObject(1,4500);
+        ps.setObject(2,2);
+        //6. 执行 sql，接受返回结果
+        int count1 = stmt.executeUpdate(sql1);
+        int count2 = ps.executeUpdate();//注意：这里直接执行，不用放SQL语句
+        //7. 处理结果
+        System.out.println("SQL1："+count1);
+        System.out.println("SQL2："+count2);
+        //8. 释放资源
         stmt.close();
+        ps.close();
         coon.close();
-
-
     }
 }
