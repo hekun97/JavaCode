f3cab6928f1c740d88bf1b11caa1a3332c4779b5
hekun97 <hekun97@outlook.com>
2021-07-16 17:15:43 +0800

    jsp and Session

--- Test/Cookie&Session/src/io/gitee/hek97/cookie/CookieTest.java
+++ Test/Cookie&Session/src/io/gitee/hek97/cookie/CookieTest.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
@@ -22,32 +23,27 @@ public class CookieDemo7 extends HttpServlet {
         //3.判断数组中是否存在Cookie内容
         //3.1存在Cookie内容
         if (cs != null && cs.length > 0) {
+            //4.设置标签判断是否存在名为lastTime的cookie，然后遍历Cookie数组
             boolean flag=false;
-            //4.遍历Cookie
             for (Cookie c : cs) {
                 //5.判断是否存在名为lastTime的cookie
                 //5.1存在名为lastTime的cookie
                 if (c.getName().equals("lastTime")) {
+                    //标签变为真
                     flag=true;
                     //5.1.1输出上次登录时间
                     response.getWriter().write("欢迎你，你上次登录的时间为：" + URLDecoder.decode(c.getValue()));
-                    //5.1.2获取当前登录的时间
-                    //获取当前时间戳
-                    Date date = new Date();
-                    //获取SimpleDateFormat对象格式化时间戳
-                    SimpleDateFormat sdf = new SimpleDateFormat();
-                    sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
-                    String format = sdf.format(date);
-                    //将时间进行URL编码
-                    String encode = URLEncoder.encode(format,"utf-8");
-                    //5.1.3将当前登录的时间存到名为lastTime的cookie中，对上次登录的时间进行更新
-                    c.setValue(encode);
+                    //5.1.2调用getCookie()的方法获取经过处理后的当前时间
+                    String nowTime = getCookie();
+                    //5.1.3将当前的时间存到名为lastTime的cookie中，对上次登录的时间进行更新
+                    c.setValue(nowTime);
+                    //5.1.4更新名为lastTime的cookie的值
                     response.addCookie(c);
-                    //5.1.4停止当前循环
+                    //5.1.5停止当前循环
                     break;
                 }
             }
-            //5.2不存在名为lastTime的cookie，用户首次登录
+            //5.2如果标签为假，不存在名为lastTime的cookie，用户首次登录
             if(flag==false){
                 firstTime(response);
             }
@@ -62,22 +58,40 @@ public class CookieDemo7 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doPost(request, response);
     }
-    //首次登录的方法
+
+    /**
+     * 用户首次登录的方法
+     * @param response lastTime的Cookie
+     * @throws IOException
+     */
     private void firstTime(HttpServletResponse response) throws IOException {
-        //输出首次登录内容
+        //1.输出首次登录内容
         response.getWriter().write("欢迎你，这是你的首次登录");
-        //获取当前时间戳
-        Date date = new Date();
-        //对时间进行格式化
-        SimpleDateFormat sdf = new SimpleDateFormat();
-        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
-        String format = sdf.format(date);
-        //将格式化后的时间进行URL编码，防止乱码
-        String encode = URLEncoder.encode(format,"utf-8");
-        //生成cookie
-        Cookie lastTime = new Cookie("lastTime", encode);
-        //添加名为lastTime的cookie到浏览器
+        //2.调用getCookie()的方法获取经过处理后的当前时间
+        String nowTime = getCookie();
+        //3.生成cookie
+        Cookie lastTime = new Cookie("lastTime", nowTime);
+        //4.设置过期时间为一个月
+        lastTime.setMaxAge(60*60*24*30);
+        //5.添加名为lastTime的cookie到浏览器
         response.addCookie(lastTime);
+    }
+
+    /**
+     * 获取当前的时间
+     * @return 处理后的当前时间
+     * @throws UnsupportedEncodingException
+     */
+    private String getCookie() throws UnsupportedEncodingException {
+        //1.获取当前时间戳
+        Date date = new Date();
+        //2.对时间进行格式化
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy年MM月dd日 HH:mm:ss");
+        String formatTime = sdf.format(date);
+        //3.将格式化后的时间进行URL编码，防止乱码
+        String nowTime = URLEncoder.encode(formatTime);
+        return nowTime;
     }
 }
 
---
+++ Test/Cookie&Session/src/io/gitee/hek97/session/SessionDemo1.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet("/SessionDemo1")
+public class SessionDemo1 extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+        this.doPost(request, response);
+    }
+}
